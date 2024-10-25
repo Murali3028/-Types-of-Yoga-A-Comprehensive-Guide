9a547b5e30bf3ee1ee162eed71de027c5aee21ff
Kakarla Murali Krishna <kakarlamurali122@gmail.com>
2024-10-25 21:49:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".header__container h2", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".header__container .btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  ScrollReveal().reveal(".header__container img", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+  ScrollReveal().reveal(".why_container .section_header", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  ScrollReveal().reveal(".why__container p", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".why__container li", {
+    ...scrollRevealOption,
+    delay: 1500,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".why__container img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+  ScrollReveal().reveal(".hero__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".classes__image", {
+    duration: 1000,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".membership__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".stories__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".posts__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  ScrollReveal().reveal(".photos__card", {
+    duration: 1000,
+    interval: 500,
+  });
